f6a98b14c6f19415b6672c0b4f52421e60eac91c
MiltonStivenPinzon1223 <miltonstivenpinzon@gmail.com>
2023-06-09 22:45:53 -0500

    xD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,9 @@
                       document.getElementById('logout-form').submit();">
          {{ __('Logout') }}
      </a>
+     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+      @csrf
+  </form>
 </li>
 </div>
 </div>
