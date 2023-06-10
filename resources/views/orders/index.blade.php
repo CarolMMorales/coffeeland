75ad305428678b795997c7df9b3d5c7bc820d444
CarolMMorales <carolmoralfs@gmail.com>
2023-06-09 23:11:29 -0500

    :v

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -2,27 +2,31 @@
 @section('content')
 <div class="container">
     <div class="table-responsive">
-        <table class="table table-dark">
+        <table class="table table-light">
             <thead>
                 <tr>
                     <th scope="col"></th>
-                    <th scope="col">Column 2</th>
-                    <th scope="col">Column 3</th>
+                    <th scope="col">Cliente</th>
+                    <th scope="col">Productos</th>
+                    <th scope="col">Cantidad</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Metodo de pago</th>
+                    <th scope="col">Orden</th>
+
                 </tr>
             </thead>
             <tbody>
                 <tr class="">
-                    <td scope="row">R1C1</td>
-                    <td>R1C2</td>
-                    <td>R1C3</td>
-                </tr>
-                <tr class="">
-                    <td scope="row">Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
+                    <td scope="row"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="btn btn-outline-success">Hecho</a></td>
                 </tr>
             </tbody>
         </table>
     </div>
-</div>    
+</div>
 @endsection
