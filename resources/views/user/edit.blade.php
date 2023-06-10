06e488cdcd454049b6d172999b87a872b34781ac
lauramontanaa <lauvmr27@gmail.com>
2023-06-09 21:21:18 -0500

    xd

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -3,9 +3,10 @@
 
         <section class="container6 container-fluid" id="booking">
           <article id="articleForms">
-            <h3>Celebrate your special occasions with us!</h3><h4>You just have to fill in the following forms.</h4>
+            <h3>Make your order right here!</h3><h4>You just have to fill in the following forms.</h4>
           </article>
           <form class="form">
+            <!--
             <div class="mb-3">
               <label for="name" class="form-label">Name</label>
               <input type="text" class="form-control" id="name" value="" placeholder="Enter your name" minlength="3" maxlenght="50" pattern="[a-zA-Z]{3-20}" title="You can only enter letters." autocomplete="off" required>
@@ -39,6 +40,43 @@
               <input type="checkbox" class="form-check-input" id="exampleCheck1">
               <label class="form-check-label" for="exampleCheck1" aria-required="true">Do you accept our terms and policies?</label>
             </div>
+            -->
+
+            
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="inputGroupSelect01">Products</label>
+              <select class="form-select" id="inputGroupSelect01">
+                <option selected>Choose a product</option>
+                <option value="1">Expresso</option>
+                <option value="2">Latte</option>
+                <option value="3">Macchiato</option>
+                <option value="4">Moccacino</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="age" class="form-label">Amount</label>
+              <input type="number" class="form-control" id="exampleInputAge" min="1" id="age">
+            </div>
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="inputGroupSelect01">Tables</label>
+              <select class="form-select" id="inputGroupSelect01">
+                <option selected>Choose a table</option>
+                <option value="1">Table 1</option>
+                <option value="2">Table 2</option>
+                <option value="3">Table 3</option>
+              </select>
+            </div>
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="inputGroupSelect01">Payment method</label>
+              <select class="form-select" id="inputGroupSelect01">
+                <option selected>Choose a payment method</option>
+                <option value="1">Debit card</option>
+                <option value="2">Cash</option>
+                <option value="3">Nequi</option>
+                <option value="4">Daviplata</option>
+              </select>
+            </div>
+
             <button type="submit" class="btn btn-secondary">Submit</button>
             </form>
         </section>
